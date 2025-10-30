081658e20c94fa631a1e83c94ae25c46ec459a1a
Abir Shaikh <shaikhabir044@gmail.com>
2025-10-30 09:06:06 +0530

    java example 2 for lambda expression

---
+++ all_file/src/Java8/LambdaExpression/Example2.java
@@ -0,0 +1,15 @@
+package Java8.LambdaExpression;
+
+public class Example2 {
+    public static void main(String[] args) {
+        MathOpn Sumopn = (a , b) -> a + b;
+        MathOpn Subopn = (a , b) -> a - b;
+        int res = Sumopn.opn(34,22);
+        int res1 =Subopn.opn(123,113);
+        System.out.println(res1);
+        System.out.println(res);
+    }
+}
+interface MathOpn {
+    int opn(int a, int b);
+}
