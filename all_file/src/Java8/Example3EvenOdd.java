98de66ffe56cd5bac71b4b96dd58d8d9cc1a7c9f
Abir Shaikh <shaikhabir044@gmail.com>
2025-11-01 18:36:34 +0530

    Java example for partitioning even odd numbers

---
+++ all_file/src/Java8/Example3EvenOdd.java
@@ -0,0 +1,14 @@
+package Java8;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//partitioning even odd
+public class Example3EvenOdd {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,12);
+        System.out.println(list.stream().collect(Collectors.partitioningBy(x -> x % 2 == 0)));
+    }
+}
