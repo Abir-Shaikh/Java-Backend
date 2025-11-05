2a80ace54f430b1764af9222fe4e685a996d0312
Abir Shaikh <shaikhabir044@gmail.com>
2025-11-05 21:03:16 +0530

    java

---
+++ all_file/src/Collection_frameworks/Queue/QueueExample.java
@@ -0,0 +1,7 @@
+package Collection_frameworks.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        //so what is Queue ? --> Its a data structure which works on FIFO concept i.e. first in first out
+    }
+}
