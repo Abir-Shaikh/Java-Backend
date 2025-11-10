fb52bb6235accb2b81b6b08bc189a7260a4f22e1
Abir Shaikh <shaikhabir044@gmail.com>
2025-11-10 08:55:55 +0530

    correction

--- all_file/src/MultiThreading/Synchronization/Counter_Synchronization.java
+++ all_file/src/MultiThreading/Synchronization/Counter_Synchronization.java
@@ -6,7 +6,7 @@ public class Counter_Synchronization {
 
     //if we dont wanna synchronized the whole method (public synchronized void increment()) then we can write it for the particular instance
     //in this case we will do it for count
-    public  void increment() {
+    public void increment() {
         synchronized (this){
             count++;
         }
